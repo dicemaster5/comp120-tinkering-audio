eac4b4b9156e0e2cb74508b1eeb8228c72244e22
dicemaster <dicemaster555@gmail.com>
2017-11-24 11:33:06 +0000

    Made a new function that can read notes from a list, the application has a song of storms melody demo

--- the 6 algorithms/4 algorithms.py
+++ the 6 algorithms/4 algorithms.py
@@ -12,17 +12,20 @@ SAMPLE_RATE = 44100.0
 BIT_DEPTH = 2.0
 CHANNELS = 2
 
-notes = {"A": 0, "A#": 1, "B": 2, "C": 3, "C#": 4, "D": 5, "D#": 6, "E": 7, "F": 8, "F#": 9, "G": 10, "G#": 11}
+notes = {"A": 0, "A#": 1, "B": 2, "C": 3, "C#": 4, "D": 5, "D#": 6, "E": 7, "F": 8, "F#": 9, "G": 10, "G#": 11,
+         "a": 12, "a#": 13, "b": 14, "c": 15, "c#": 16, "d": 17, "d#": 18, "e": 19, "f": 20, "f#": 21, "g": 22, "g#": 23}
+sound_file = []
+
 
 def change_volume(samples,volume_change):
     for sample in samples:
         numpy.multiply(sample, volume_change, out=sample, casting="unsafe")
 
-def generate_tone_from_string(note,sample_rate, seconds, volume):
 
+def generate_tone_from_string(note,sample_rate, seconds, volume):
     values = []
     frequency = 440.0 * 2.0 ** (notes[note] / 12.0)
-    sample_length = seconds * 44100
+    sample_length = int(seconds * 44100)
     for i in range(0, sample_length):
         value = math.sin(2 * math.pi * frequency * (i / sample_rate)) * (volume * BIT_DEPTH)
         for j in xrange(0, CHANNELS):
@@ -31,16 +34,15 @@ def generate_tone_from_string(note,sample_rate, seconds, volume):
     return values
 
 
-def create_melody_from_tones(list_of_tones):
-    for i in list_of_tones:
-        for value in i:
-
-            list_of_tones[0].append(i)
+def write_melody(list_of_notes):
+    sound_file.extend(generate_tone_from_string(list_of_notes[0], SAMPLE_RATE, 0.6, 1000.0))
+    for note in list_of_notes[1:]:
+        sound_file.extend(generate_tone_from_string(note, SAMPLE_RATE, 0.6, 1000.0))
 
 
 def save_melody(wav_data, name_of_file):
-    packed_values=[]
-    for i in range(0,len(wav_data)):
+    packed_values = []
+    for i in range(0, len(wav_data)):
         packed_value = struct.pack('h', wav_data[i])
         packed_values.append(packed_value)
 
@@ -51,20 +53,8 @@ def save_melody(wav_data, name_of_file):
     noise_out.close()
 
 
+list_of_notes = ['D', 'F', 'd', 'D', 'F', 'd', 'e', 'f', 'e', 'f', 'e', 'c', 'A', 'A', 'D', 'F', 'G', 'A', 'A', 'D', 'F', 'G', 'E']
 
-note1 = generate_tone_from_string('D', SAMPLE_RATE, 1, 1000.0)
-note2 = generate_tone_from_string('F', SAMPLE_RATE, 5, 1000.0)
-note3 = generate_tone_from_string('D', SAMPLE_RATE, 2, 1000.0)
-note4 = generate_tone_from_string('D', SAMPLE_RATE, 1, 1000.0)
-note5 = generate_tone_from_string('F', SAMPLE_RATE, 3, 1000.0)
-note6 = generate_tone_from_string('D', SAMPLE_RATE, 1, 1000.0)
-note7 = generate_tone_from_string('E', SAMPLE_RATE, 1, 1000.0)
+write_melody(list_of_notes)
 
-#melody_list = [note1, note2, note3, note4]
-new_melody = note1.extend(note2)
-new_melody = note1.extend(note3)
-new_melody = note1.extend(note4)
-new_melody = note1.extend(note5)
-new_melody = note1.extend(note6)
-new_melody = note1.extend(note7)
-save_melody(note1, "newMelody.wav")
+save_melody(sound_file, "newMelody.wav")
